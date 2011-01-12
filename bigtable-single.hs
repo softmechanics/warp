c40b8dfc13c99b006e77e6d1030251f9047a4001
Matt Brown <matt@softmechanics.net>
2011-01-12 15:28:20 -0800

    make bigtable a little bigger

--- bigtable-single.hs
+++ bigtable-single.hs
@@ -4,14 +4,15 @@ import Blaze.ByteString.Builder (Builder, fromByteString)
 import Blaze.ByteString.Builder.Char8 (fromShow)
 import Data.Monoid (mappend)
 import Network.Wai.Handler.Warp (run)
+import Data.List (foldl')
 
 bigtable :: Builder
 bigtable =
     fromByteString "<table>"
-    `mappend` foldr mappend (fromByteString "</table>") (replicate 2 row)
+    `mappend` foldl' mappend (fromByteString "</table>") (replicate 200 row)
   where
     row = fromByteString "<tr>"
-          `mappend` foldr go (fromByteString "</tr>") [1..2]
+          `mappend` foldr go (fromByteString "</tr>") [1..10]
     go i rest = fromByteString "<td>"
                 `mappend` fromShow i
                 `mappend` fromByteString "</td>"
