3399050028f9b101c6c791cb963b0e8df48c8c9c
Matt Brown <matt@softmechanics.net>
2011-01-13 23:11:36 -0800

    bigger bigtable tables

--- bigtable-single.hs
+++ bigtable-single.hs
@@ -1,18 +1,19 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, CPP #-}
 import Network.Wai
 import Blaze.ByteString.Builder (Builder, fromByteString)
 import Blaze.ByteString.Builder.Char8 (fromShow)
 import Data.Monoid (mappend)
 import Network.Wai.Handler.Warp (run)
-import Data.List (foldl')
+import Data.List (foldl1')
 
 bigtable :: Builder
 bigtable =
     fromByteString "<table>"
-    `mappend` foldl' mappend (fromByteString "</table>") (replicate 200 row)
+    `mappend` foldl1' mappend (replicate 1000 row)
+    `mappend` fromByteString "</table>"
   where
     row = fromByteString "<tr>"
-          `mappend` foldr go (fromByteString "</tr>") [1..10]
+          `mappend` foldr go (fromByteString "</tr>") [1..50]
     go i rest = fromByteString "<td>"
                 `mappend` fromShow i
                 `mappend` fromByteString "</td>"
@@ -20,4 +21,9 @@ bigtable =
 
 main = run 3000 app
 
-app _ = return $ ResponseBuilder status200 [("Content-Type", "text/html")] bigtable
+app _ = return $ ResponseBuilder status200 [
+      ("Content-Type", "text/html")
+#ifndef CHUNKED_RESPONSE
+    , ("Content-Length", B.pack "22015")
+#endif
+    ] bigtable
--- bigtable-stream.hs
+++ bigtable-stream.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, CPP #-}
 import Network.Wai
 import Blaze.ByteString.Builder (Builder, fromByteString)
 import Blaze.ByteString.Builder.Char8 (fromShow)
@@ -9,10 +9,10 @@ import Data.Enumerator (enumList, ($$), run_)
 bigtable :: [Builder]
 bigtable =
     fromByteString "<table>"
-    : foldr (:) [fromByteString "</table>"] (replicate 2 row)
+    : foldr (:) [fromByteString "</table>"] (replicate 1000 row)
   where
     row = fromByteString "<tr>"
-          `mappend` foldr go (fromByteString "</tr>") [1..2]
+          `mappend` foldr go (fromByteString "</tr>") [1..50]
     go i rest = fromByteString "<td>"
                 `mappend` fromShow i
                 `mappend` fromByteString "</td>"
@@ -21,4 +21,9 @@ bigtable =
 main = run 3000 app
 
 app _ = return $ ResponseEnumerator $ \f ->
-    run_ $ enumList 4 bigtable $$ f status200 [("Content-Type", "text/html")]
+    run_ $ enumList 1 bigtable $$ f status200 [
+      ("Content-Type", "text/html")
+#ifndef CHUNKED_RESPONSE
+    , ("Content-Length", "22015")
+#endif
+    ]
